a39f9a55b00726ce706e136b4cf92e8227a2065b
Christina Moore <sayhitochristina@gmail.com>
2018-02-06 10:43:55 -0600

    4-7

--- drills.js
+++ drills.js
@@ -33,14 +33,48 @@ process.stdout.write('\x1Bc');
 //   .then(results => console.log(results));
 
 //Number 3
+// knex
+//   .select('id', 'name')
+//   .from('restaurants')
+//   .where('cuisine', 'Italian')
+//   .limit(10)
+//   .then(results => console.log(results));
+
+// Number 4
+// knex('restaurants')
+//   .count('cuisine')
+//   .where('cuisine', 'Thai')
+//   .then(results => console.log(results));
+ 
+
+// Number 5
+// knex('restaurants')
+//   .count('restaurants')
+//   .then(consoleLog);
+
+// Number 6
+// knex('restaurants')
+//   .count()
+//   .where('cuisine', 'Thai')
+//   .where('address_zipcode', '11372')
+//   .then(consoleLog);
+
+// Number 7
 knex
   .select('id', 'name')
   .from('restaurants')
-  .where('cuisine', 'Italian')
-  .limit(10)
-  .then(results => console.log(results));
+  .whereIn('address_zipcode', [10012, 10013, 10014])
+  .orderBy('name', 'asc')
+  .limit(5)
+  .then(consoleLog);
+  
 
 // Destroy the connection pool
 knex.destroy().then(() => {
   console.log('database connection closed');
 });
+
+// HELPER 
+function consoleLog (res) {
+  console.log(res);
+}
