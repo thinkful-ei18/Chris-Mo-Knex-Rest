e75eac4745c591f7bf885502603ad998a95e1e67
admin <Mistermo716@gmail.com>
2018-02-06 08:15:29 -0800

    KNEX Drills

--- drills.js
+++ drills.js
@@ -1,19 +1,44 @@
 'use strict';
 
 const { DATABASE } = require('./config');
-const knex = require('knex')(DATABASE);
+
+const knex = require('knex')({
+  client: 'pg',
+  connection: {
+      database: 'dev-restaurants-app'
+  },
+});
 
 // clear the console before each run
 process.stdout.write('\x1Bc');
 
 // Sample select 
-knex
-  .select()
-  .from('restaurants')
-  .limit(2)
-  .debug(true)
-  .then(results => console.log(results));
+// knex
+//   .select()
+//   .from('restaurants')
+//   .limit(2)
+//   .debug(true)
+//   .then(results => console.log(results));
+//Number 1
+// knex 
+//   .select()
+//   .from('restaurants')
+//   .then(results => console.log(results));
 
+//Number 2
+// knex
+//   .select()
+//   .from('restaurants')
+//   .where('cuisine','Italian')
+//   .then(results => console.log(results));
+
+//Number 3
+knex
+  .select('id', 'name')
+  .from('restaurants')
+  .where('cuisine', 'Italian')
+  .limit(10)
+  .then(results => console.log(results));
 
 // Destroy the connection pool
 knex.destroy().then(() => {
